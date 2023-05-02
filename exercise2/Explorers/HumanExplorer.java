bd51012c73ba34ec1a9df16ddbf45f84b652d28e
NielAnasco <122077231+NielAnasco@users.noreply.github.com>
2023-05-02 11:26:08 +0800

    Create HumanExplorer.java

---
+++ exercise2/Explorers/HumanExplorer.java
@@ -0,0 +1,15 @@
+public class HumanExplorer implements Explorer {
+    
+    public void visit(Earth earth) {
+        System.out.println("Deploying Wall-E specific to Earth... exploring life");
+    }
+
+    public void visit(Uranus uranus) {
+        System.out.println("Deploying Autobots specific to Uranus... exploring life");
+    }
+
+}
+
+
+   
+    
